dae168fc98fa59277ab4e70a02a0c7d22e8d0ac0
h600879 <600879@stud.hvl.no>
2023-03-14 14:26:52 +0100

    Fixed react routes on TomEE

--- src/main/app/src/App.js
+++ src/main/app/src/App.js
@@ -1,34 +1,14 @@
-import {useEffect, useState} from "react";
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Test from "./components/test";
 
 function App() {
-
-  const [Data, setData] = useState([]);
-  const DemoDataFraSping = async () => {
-    await fetch(process.env.REACT_APP_PROXY_HOST + '/api/demo', {method: "GET"},)
-        .then((respons) => respons.json())
-        .then((data) => {
-          setData(data);
-        }).catch((err) => console.log(err));
-
-
-  }
-
-  useEffect(() => {
-    DemoDataFraSping();
-  }, [])
-
   return (
-      <div>
-        <div>
-          Hello World from React!
-        </div>
-        <div>
-          {Data.map(item => item + " ")}
-        </div>
-      </div>
-
-
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Routes>
+              <Route index element={<Test />} />
+              <Route path="/test" element={<div>Test</div>} />
+          </Routes>
+      </BrowserRouter>
   );
 }
 export default App;
