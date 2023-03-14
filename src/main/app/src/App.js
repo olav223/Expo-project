705669fcd50db33bce43d8e7960cec90514ddfb2
h600879 <600879@stud.hvl.no>
2023-03-14 14:26:52 +0100

    fIrSt CoMmIt

--- src/main/app/src/App.js
+++ src/main/app/src/App.js
@@ -1,34 +1,13 @@
-import {useEffect, useState} from "react";
+import {Route, Routes} from "react-router-dom";
+import Test from "./components/test";
 
 
 function App() {
-
-  const [Data, setData] = useState([]);
-  const DemoDataFraSping = async () => {
-    await fetch(process.env.REACT_APP_PROXY_HOST + '/api/demo', {method: "GET"},)
-        .then((respons) => respons.json())
-        .then((data) => {
-          setData(data);
-        }).catch((err) => console.log(err));
-
-
-  }
-
-  useEffect(() => {
-    DemoDataFraSping();
-  }, [])
-
   return (
-      <div>
-        <div>
-          Hello World from React!
-        </div>
-        <div>
-          {Data.map(item => item + " ")}
-        </div>
-      </div>
-
-
+      <Routes>
+          <Route index element={<Test />} />
+          <Route path="test" element={<div>Test</div>} />
+      </Routes>
   );
 }
 export default App;
