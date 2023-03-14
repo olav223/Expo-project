a1f26823e7e7f11b118f8ab41dec1e358dde70f9
594760 <594760@stud.hvl.no>
2023-03-14 11:54:27 +0100

    Changes by hanshammersland

---
+++ src/main/java/no/hvl/dat109/expoproject/interfaces/utils/IVoteUtil.java
@@ -0,0 +1,10 @@
+package no.hvl.dat109.expoproject.interfaces.utils;
+
+public interface IVoteUtil {
+
+    /**
+     * generates a random and unique ID
+     * @return generated voter ID
+     */
+    String generateVoterID();
+}
