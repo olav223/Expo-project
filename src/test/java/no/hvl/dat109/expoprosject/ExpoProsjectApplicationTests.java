c2777891a9acf26d675a68483be37521c818bfea
594760 <594760@stud.hvl.no>
2023-03-14 10:49:35 +0100

    Start

---
+++ src/test/java/no/hvl/dat109/expoprosject/ExpoProsjectApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat109.expoprosject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpoProsjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
