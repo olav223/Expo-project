c70c49f0b8b23d3756aecacddb9939c57244a667
Martin Berg Alstad <600878@stud.hvl.no>
2023-03-16 14:36:48 +0100

    StandController skal implementere riktig interface

--- src/main/java/no/hvl/dat109/expoproject/controllers/StandController.java
+++ src/main/java/no/hvl/dat109/expoproject/controllers/StandController.java
@@ -1,33 +1,32 @@
 package no.hvl.dat109.expoproject.controllers;
 
-import no.hvl.dat109.expoproject.database.StandService;
 import no.hvl.dat109.expoproject.entities.Stand;
+import no.hvl.dat109.expoproject.interfaces.controllers.IStandController;
 import no.hvl.dat109.expoproject.interfaces.database.IStandService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/stand")
-public class StandController implements IStandService {
+public class StandController implements IStandController {
 
     private final IStandService ss;
 
-    public StandController(StandService ss) {
+    public StandController(IStandService ss) {
         this.ss = ss;
     }
 
     @Override
-    public void addStand(Stand stand) {
-
-    }
-
-    @Override
-    public void updateStand(Stand stand) {
-
-    }
-
-    @Override
-    public Stand removeStand(int standID) {
+    @GetMapping
+    public Stand getStand(int id) {
         return null;
+    }
+
+    @Override
+    @PostMapping
+    public void postUpdateInfo(Stand stand) {
+
     }
 }
