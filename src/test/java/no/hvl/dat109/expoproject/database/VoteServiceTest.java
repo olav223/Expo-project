c11534b37b378d137292ecf1c53e367b4e4a4446
Martin Berg Alstad <600878@stud.hvl.no>
2023-03-20 17:58:51 +0100

    Fjernet ubrukte imports

--- src/test/java/no/hvl/dat109/expoproject/database/VoteServiceTest.java
+++ src/test/java/no/hvl/dat109/expoproject/database/VoteServiceTest.java
@@ -15,8 +15,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
