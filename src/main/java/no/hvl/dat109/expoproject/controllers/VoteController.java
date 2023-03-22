46fad48c60834f41d4b1ce0a53b75f26f026a14f
Martin Berg Alstad <600878@stud.hvl.no>
2023-03-22 10:34:53 +0100

    Fjernet metoder relatert til MVC

--- src/main/java/no/hvl/dat109/expoproject/controllers/VoteController.java
+++ src/main/java/no/hvl/dat109/expoproject/controllers/VoteController.java
@@ -1,6 +1,5 @@
 package no.hvl.dat109.expoproject.controllers;
 
-import no.hvl.dat109.expoproject.database.EventService;
 import no.hvl.dat109.expoproject.database.VoteService;
 import no.hvl.dat109.expoproject.database.VoterRepo;
 import no.hvl.dat109.expoproject.entities.Event;
@@ -8,7 +7,7 @@ import no.hvl.dat109.expoproject.entities.StandWithVote;
 import no.hvl.dat109.expoproject.entities.Vote;
 import no.hvl.dat109.expoproject.entities.Voter;
 import no.hvl.dat109.expoproject.interfaces.controllers.IVoteController;
-import org.springframework.beans.factory.annotation.Autowired;
+import no.hvl.dat109.expoproject.interfaces.database.IVoteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -19,38 +18,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/vote")
 public class VoteController implements IVoteController {
-    @Autowired
-    private VoteService vs;
-    @Autowired
-    private EventService es;
-    @Autowired
-    private VoterRepo vr;
+    private final IVoteService vs;
+    private final VoterRepo vr; // TODO bør ikke bruke repo utenfor service
 
-    /**
-     * First site the guests land on after scanning qr code
-     * @return the first site
-     */
-    @GetMapping
-    public String getMapping(){
-        //Enter one-time code site
-        return "api/vote";
+    public VoteController(VoteService vs, VoterRepo vr) {
+        this.vs = vs;
+        this.vr = vr;
     }
 
-    /**
-     * Gives the view for the stand the voter wants to vote for
-     * @param voterid The one time code for the voter
-     * @param eventId the id for the event
-     * @param standid the stand that is voted for
-     * @return the voting site for the stand if the code is correct, redirects back otherwise
-     */
-    @GetMapping("/stand")
-    public String veiwStand(@RequestParam("voterId") String voterid, @RequestParam("event") int eventId, @RequestParam("stand") int standid){
-        Event event = es.findEventById(eventId);
-        if(validVoterID(voterid, event))
-            return "api/vote/stand/" + standid;
-
-        return "redirect:api/vote";
-    }
     /**
      * Registrerer en stemme
      *
@@ -96,7 +71,7 @@ public class VoteController implements IVoteController {
      * @throws ResponseStatusException hvis voterID eller standID er tomme eller ikke eksisterende
      */
     @Override
-    @GetMapping("/any")
+    @GetMapping
     public int getVote(@RequestParam String voterID, @RequestParam int standID) {
         if (voterID.equals("") || standID == 0) {
             throw new ResponseStatusException(HttpStatus.NO_CONTENT, "VoterID or standID cannot be empty or 0");
@@ -122,17 +97,18 @@ public class VoteController implements IVoteController {
 
     /**
      * Checking if the code passed for the voter is valid
+     *
      * @param voterId the 6-digit code for the voter
-     * @param event the event the code is assigned
+     * @param event   the event the code is assigned
      * @return true if the code exists, false otherwise
      */
-    @Override
+    @Override // TODO sett opp mapping, hvis det trengs
     public boolean validVoterID(String voterId, Event event) {
         List<Voter> voters = vr.findAllByEvent(event);
         Voter voter = new Voter(voterId, event);
 
-        for(Voter voter1 : voters){
-            if(voter.equals(voter1)){
+        for (Voter voter1 : voters) {
+            if (voter.equals(voter1)) {
                 return true;
             }
         }
