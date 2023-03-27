78daad4504b642e8c847b8ce386694251b440cf5
trbjrn <573763@stud.hvl.no>
2023-03-27 10:14:37 +0200

    Started med å lage admin side

--- src/main/app/src/App.tsx
+++ src/main/app/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import AdminEvent from "./pages/admin/AdminEvent";
 import FrontPage from "./pages/FontPage";
 import StandPage from "./pages/StandPage";
 
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route index element={<FrontPage />}/>
       <Route path="/stand" element={<StandPage />} />
+      <Route path="/admin" element={<AdminEvent/>}/>
     </Routes>
   </BrowserRouter>
 }
--- src/main/app/src/pages/admin/AdminEvent.tsx
+++ src/main/app/src/pages/admin/AdminEvent.tsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+import restApi from "../../utils/restApi";
+
 const AdminEvent = () => {
-    return <div>AdminEvent</div>
+    
+    const [events, setEvents] = useState([]);
+    
+    const getEvents =async () => {
+        const result = await restApi({url : "/api/event/admin?username=exhibitor1", method : "GET"})
+        if(result.status === 200){
+            setEvents(result.body)
+        }
+    }
+    
+    useEffect(() => {
+        getEvents();
+    }, [])
+    
+    return (
+        <div className="standList">
+        {events.length > 0 ? events.map((item,i) => {
+            return <div key={"event-"+i} className="standItem">
+                <div>
+                    <h4>{item["title"]}</h4>
+                    <p>{item["description"]}</p>
+                </div>
+            </div>
+        }) : <div>Ingen Arrengementer</div>}
+    </div>
+    )
 }
+
 
 export default AdminEvent;
