f164d4c1e74e185c66c4823dce7261b60330b11a
trbjrn <573763@stud.hvl.no>
2023-03-27 10:08:57 +0200

    Utvidet Accesslevel.java

--- src/main/java/no/hvl/dat109/expoproject/enums/AccessLevel.java
+++ src/main/java/no/hvl/dat109/expoproject/enums/AccessLevel.java
@@ -1,7 +1,12 @@
 package no.hvl.dat109.expoproject.enums;
 
 public enum AccessLevel {
-    ADMIN,
-    JURY,
-    EXIBITOR
+    ADMIN(0),
+    JURY(1),
+    EXIBITOR(2);
+
+    private final int level;
+    AccessLevel(int level){
+        this.level = level;
+    }
 }
