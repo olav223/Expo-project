ee898609a4baa77b29d846f9d6f6eb2f3527e3c7
Martin Berg Alstad <600878@stud.hvl.no>
2023-03-28 12:35:36 +0200

    Lagt inn route for JuryPage

--- src/main/app/src/App.tsx
+++ src/main/app/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import FrontPage from "./pages/FontPage";
 import StandPage from "./pages/StandPage";
+import JuryPage from "./pages/jury/JuryPage";
 
 function App() {
   return <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route index element={<FrontPage />}/>
       <Route path="/stand" element={<StandPage />} />
+      <Route path={"/jury"} element={<JuryPage />} />
     </Routes>
   </BrowserRouter>
 }
