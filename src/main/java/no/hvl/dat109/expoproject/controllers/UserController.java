ac74b73018ccf5e646ab2d24c256dbf8fe36b34d
oyvindinst <oyvind.instefjord12@gmail.com>
2023-03-28 11:42:15 +0200

    Ferdig med userController

--- src/main/java/no/hvl/dat109/expoproject/controllers/UserController.java
+++ src/main/java/no/hvl/dat109/expoproject/controllers/UserController.java
@@ -1,12 +1,18 @@
 package no.hvl.dat109.expoproject.controllers;
 
 import no.hvl.dat109.expoproject.database.UserService;
+import no.hvl.dat109.expoproject.entities.Event;
 import no.hvl.dat109.expoproject.entities.User;
 import no.hvl.dat109.expoproject.interfaces.controllers.IUserController;
 import no.hvl.dat109.expoproject.interfaces.database.IUserService;
+import no.hvl.dat109.expoproject.utils.PasswordUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+
+import static no.hvl.dat109.expoproject.utils.PasswordUtils.validate;
 
 @RestController
 @RequestMapping("/api/user")
@@ -21,10 +27,10 @@ public class UserController implements IUserController {
     @Override
     @GetMapping
     public User getUser(@RequestParam(defaultValue = "") String username) {
-    if(username.equals("")){
-       return null;
-    }
-    return us.getUser(username);
+        if (username.equals("")) {
+            return null;
+        }
+        return us.getUser(username);
     }
 
     @Override
@@ -35,8 +41,20 @@ public class UserController implements IUserController {
 
     @Override
     @PostMapping("/login")
-    public int postLogin(String username, String password) {
+    public int postLogin(@RequestHeader String username, @RequestHeader String password) {
+        User user = us.getUser(username);
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "username or password is wrong");
+        } else {
+            String salt = user.getSalt();
+            if (!validate(password, salt, user.getHash())) {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "username or password is wrong");
+            } else {
+                return user.getAccessLevel();
+            }
 
-        return 0;
+        }
     }
+
+
 }
