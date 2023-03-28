16c5a383ad30582fe5cbb48dc5587a6243f64dbe
h600879 <600879@stud.hvl.no>
2023-03-28 14:22:40 +0200

    Fixed cookies and cookie checks in frontend

--- src/main/app/src/utils/auth.tsx
+++ src/main/app/src/utils/auth.tsx
@@ -1,9 +1,28 @@
-export default class Auth {
-    signIn():void {
+import Cookies from "universal-cookie";
+import restApi from "./restApi";
 
+const cookies = new Cookies();
+
+export default class Auth {
+    async createVoter(eventId:string):Promise<boolean> {
+        const result = await restApi({url:"/api/vote/newvoter?eventID="+eventId,method:"GET"});
+        if (result.status == 200 && result.body) {
+            const expireDay = new Date();
+            expireDay.setDate(expireDay.getDate() + 7);
+            cookies.set('user', result.body["id"], { path: '/', expires: expireDay });
+            return true;
+        }
+        return false;
     }
 
-    getUser():UserModel {
-        return {username: "1"}
+    getUser():Voter | null {
+        const voter = cookies.get("user");
+        if (voter) return {id: voter};
+        return null;
+    }
+
+    async verifyUser(id:string):Promise<boolean> {
+        const result = await restApi({url:"/api/vote/validate?voterId="+id,method:"GET"});
+        return result.status == 200 && result.body;
     }
 }
