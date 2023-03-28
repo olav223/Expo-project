5ff8ff53daa6d5719503ee4f0bffe82e4b96f85f
Martin Berg Alstad <600878@stud.hvl.no>
2023-03-28 16:17:30 +0200

    Merget med master. Lagt inn manglende imports i App.tsx

--- src/main/app/src/App.tsx
+++ src/main/app/src/App.tsx
@@ -1,21 +1,28 @@
 import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FrontPage from "./pages/FontPage";
 import StandPage from "./pages/StandPage";
+import LoginPage from "./pages/LoginPage";
+import AdminEvent from "./pages/admin/AdminEvent";
+import AdminPage from "./pages/admin/AdminPage";
+import AdminStandEdit from "./pages/admin/AdminStandEdit";
+import AdminEventEdit from "./pages/admin/AdminEventEdit";
+import JuryPage from "./pages/jury/JuryPage";
 
 function App() {
-  return <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <Routes>
-      <Route index element={<FrontPage />}/>
-      <Route path="/stand" element={<StandPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/admin" element={<AdminPage />} />
-      <Route path="/admin/event" element={<AdminEvent />} />
-      <Route path="/admin/event/edit" element={<AdminEventEdit />} />
-      <Route path="/admin/stand/edit" element={<AdminStandEdit />} />
-      <Route path="/stand" element={<StandPage />} />
-      <Route path={"/jury"} element={<JuryPage />} />
-    </Routes>
-  </BrowserRouter>
+    return <BrowserRouter basename={ process.env.PUBLIC_URL }>
+        <Routes>
+            <Route index element={ <FrontPage /> } />
+            <Route path="/stand" element={ <StandPage /> } />
+            <Route path="/login" element={ <LoginPage /> } />
+            <Route path="/admin" element={ <AdminPage /> } />
+            <Route path="/admin/event" element={ <AdminEvent /> } />
+            <Route path="/admin/event/edit" element={ <AdminEventEdit /> } />
+            <Route path="/admin/stand/edit" element={ <AdminStandEdit /> } />
+            <Route path="/stand" element={ <StandPage /> } />
+            <Route path={ "/jury" } element={ <JuryPage /> } />
+        </Routes>
+    </BrowserRouter>
 }
+
 export default App;
