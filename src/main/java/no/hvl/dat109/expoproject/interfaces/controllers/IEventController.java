df8d02f36e86cb568c86131c08dcafbaf05aa1bc
trbjrn <573763@stud.hvl.no>
2023-03-29 11:01:42 +0200

    Slettet to klasser som ikke trengtes

--- src/main/java/no/hvl/dat109/expoproject/interfaces/controllers/IEventController.java
+++
@@ -1,9 +0,0 @@
-package no.hvl.dat109.expoproject.interfaces.controllers;
-
-import no.hvl.dat109.expoproject.entities.Event;
-
-import java.util.List;
-
-public interface IEventController {
-    List<Event> getEventsByAdmin(String username);
-}
