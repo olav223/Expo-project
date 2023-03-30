ffd98781a1df8dacf8cd7342f372b72aada73c20
trbjrn <573763@stud.hvl.no>
2023-03-30 22:22:53 +0200

    Lage en link til /home i logo

--- src/main/app/src/components/Navbar/Navbar.tsx
+++ src/main/app/src/components/Navbar/Navbar.tsx
@@ -2,7 +2,9 @@ import "./Navbar.css"
 import {Link} from "react-router-dom";
 const Navbar = () => {
 return <nav className={"box"}>
-        <img alt="logo" src="https://www.hvl.no/globalassets/hvl-internett/it/logo/hvl_logo.png" />
+        <Link to="/" className="home-link">
+            <img alt="logo" src="https://www.hvl.no/globalassets/hvl-internett/it/logo/hvl_logo.png" />
+        </Link>
         <ul className={"ui-menu"}>
             <Link to={"/"}><li>Hjem</li></Link>
             <Link to={"/stands"}><li>Stand</li></Link>
