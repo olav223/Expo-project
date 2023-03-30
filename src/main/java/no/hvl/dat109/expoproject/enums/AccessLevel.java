f43c41eba760cbc8545fa5cc82396a45d01351a1
trbjrn <573763@stud.hvl.no>
2023-03-30 13:42:47 +0200

    Restored file

--- src/main/java/no/hvl/dat109/expoproject/enums/AccessLevel.java
+++ src/main/java/no/hvl/dat109/expoproject/enums/AccessLevel.java
@@ -1,12 +1,7 @@
 package no.hvl.dat109.expoproject.enums;
 
 public enum AccessLevel {
-    ADMIN(0),
-    JURY(1),
-    EXIBITOR(2);
-
-    private final int level;
-    AccessLevel(int level){
-        this.level = level;
-    }
+    ADMIN,
+    JURY,
+    EXIBITOR
 }
