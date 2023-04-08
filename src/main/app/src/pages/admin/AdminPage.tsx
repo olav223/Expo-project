1f9d86a92a564c6be107db3c8845263498ee6a07
h600879 <600879@stud.hvl.no>
2023-04-08 09:53:56 +0200

    Stashed

--- src/main/app/src/pages/admin/AdminPage.tsx
+++ src/main/app/src/pages/admin/AdminPage.tsx
@@ -1,5 +1,8 @@
+import EventList from "../../components/EventList/EventList";
+
 const AdminPage = () => {
     return <div>
+        <EventList />
         <p>TODO: import Eventlist komponent</p>
         <p>- endre EventList til å ta inn en lenke url, slik at den kan navigere til edit event siden med id-en</p>
     </div>
