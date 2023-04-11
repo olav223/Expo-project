d9455bf16cc1065e74db945d1b8d2d22fcadf57e
599007 <599007@stud.hvl.no>
2023-04-11 12:27:52 +0200

    Gjort ferdig stand info sida med layout og css

--- src/main/app/src/pages/exhibitor/ExhibitorEditPage.tsx
+++ src/main/app/src/pages/exhibitor/ExhibitorEditPage.tsx
@@ -1,5 +1,68 @@
+import {Link} from "react-router-dom";
+import { useState } from "react";
 const ExhibitorEditPage = () => {
-    return <div>ExhibitorEditPage</div>
-}
+    const [standName, setStandName] = useState("");
+    const [standDescription, setStandDescription] = useState("");
+    const [standImage, setStandImage] = useState(null);
+    const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files ? event.target.files[0] : null; // add null check
+        if (file) {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+                const imageData = reader.result;
+                console.log(imageData);
+            }
+        }
+    }
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
 
+        // Create a FormData object to send the data as multipart/form-data
+        const formData = new FormData();
+        formData.append("standName", standName);
+        formData.append("standDescription", standDescription);
+        if (standImage) {
+            formData.append("standImage", standImage);
+        }
+        try {
+            const response = await fetch("/api/stands", {
+                method: "POST",
+                body: formData,
+            });
+            if (response.ok) {
+                console.log("Stand data uploaded successfully");
+            } else {
+                console.error("Failed to upload stand data");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    return  <div>
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
+            <div style={{marginTop: "10px" }}>
+            <Link style={{position: "absolute", left: "10px"}} to="/Exhibitor">
+                <button type={"submit"}>Exhibitor page</button>
+            </Link>
+                </div>
+            <h1 style={{textAlign:"center", width: "fit-content"}}>Exhibitor edit page</h1>
+            <br/>
+            <form onSubmit={handleSubmit}>
+                <input type="text" style={{border: "1px solid black", padding: "5px"}} placeholder={"Enter stand name here:"} />
+                <br />
+                <textarea
+                    rows={5}
+                    cols={50}
+                    style={{ border: "1px solid black", padding: "5px", height: "200px", width: "600px" }}
+                    placeholder="Enter your stand description here:"
+                ></textarea>
+                <br />
+                <input type="file" onChange={handleImageUpload} />
+                <br />
+                <button type="submit">Upload Stand Data</button>
+            </form>
+        </div>
+    </div>
+}
 export default ExhibitorEditPage;
--- src/main/app/src/pages/exhibitor/ExhibitorPage.tsx
+++ src/main/app/src/pages/exhibitor/ExhibitorPage.tsx
@@ -1,5 +1,61 @@
-const ExhibitorPage = () => {
-    return <div>ExhibitorPage</div>
-}
+import {Link, Router, Routes, useLocation} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import ExhibitorEditPage from "./ExhibitorEditPage";
+// @ts-ignorem
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import {StandModel} from "../../model/Stand";
+import restApi from "../../utils/restApi";
+import {useParams} from "react-router";
 
+
+interface StandData {
+    standName: string;
+    standDescription: string;
+    standImage: string;
+}
+const ExhibitorPage = () => {
+    const [stand, setExhibitor] = useState<StandModel | null>(null)
+
+    useEffect(() => {
+        getExhibitor();
+    }, []);
+
+    const {id} = useParams()
+
+    const getExhibitor = async() => {
+        const result = await restApi({url : `/api/stand?id=${id}`, method : "GET"})
+
+        if(result.status === 200){
+            setExhibitor(result.body)
+        }
+    }
+    const update = async () => {
+
+        await restApi({url : `/api/stand/update`, method : "POST", body :
+                {
+                    "id" : id,
+                    "title" : stand!.title,
+                    "description" : stand!.description,
+                    "image" : stand!.image,
+                    "url" : stand!.url,
+                    "eventID" : stand!.eventID,
+                    "responsibleID" : stand!.responsibleID
+                }
+        })}
+
+    return <div>
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
+            <div style={{marginTop: "10px" }}>
+            <Link style={{position: "absolute", right: "10px"}} to="edit">
+                <button type={"submit"}>Exhibitor edit page</button>
+            </Link>
+                </div>
+            <h1 style={{textAlign:"center", width: "fit-content"}}>ExhibitorPage</h1>
+            <input type="text" placeholder={`${stand?.title}`} onChange={(e) => stand!.title = e.target.value}/>
+            <input type="text" placeholder={`${stand?.description}`} onChange={(e) => stand!.description = e.target.value}/>
+            <input type="text" placeholder={`${stand?.image}`} onChange={(e) => stand!.image = e.target.value}/>
+            <input type="text" placeholder={`${stand?.title}`} onChange={(e) => stand!.title = e.target.value}/>
+        </div>
+    </div>
+}
 export default ExhibitorPage;
