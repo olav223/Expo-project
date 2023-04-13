75fc595f0705d8e8f5ce6fbe7d1fbf046e6f4fce
Martin Berg Alstad <600878@stud.hvl.no>
2023-04-13 10:40:26 +0200

    Lagt inn route for vurderinger fra admin

--- src/main/app/src/components/AdminEvents/AdminEvents.tsx
+++ src/main/app/src/components/AdminEvents/AdminEvents.tsx
@@ -1,41 +1,46 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import restApi from "../../utils/restApi";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./AdminEvents.css";
 
 const AdminEvents = () => {
     const [events, setEvents] = useState<Array<EventModel>>([]);
 
-    const getEvents = async() => {
-        const result = await restApi({url: "/api/admin/events/all", method: "GET"});
+    const getEvents = async () => {
+        const result = await restApi({ url: "/api/admin/events/all", method: "GET" });
         if (result.status === 200) {
-            setEvents(result.body)
+            setEvents(result.body);
         }
-    }
+        else {
+            console.error("Error fetching events: " + result);
+        }
+    };
 
     useEffect(() => {
         getEvents();
     }, []);
 
-    return events.length > 0 ?<>
-        <div className={"admin-events-parent"}>
+    return events.length > 0 ? (
+        <div className={ "admin-events-parent" }>
             <h2>Hendelser du administrerer</h2>
-            {events.map((event) => {
-                return <div className={"event-item box"} >
-                    <div className={"image-layer"} style={{backgroundImage:`url(${event.image})`}}>
-                        <div className={"overlay"}></div>
-                        <div className={"content center"}>
-                            <h2>{event.name}</h2>
+            { events.map((event) =>
+                <div className={ "event-item box" } key={ event.id }>
+                    <div className={ "image-layer" } style={ { backgroundImage: `url(${ event.image })` } }>
+                        <div className={ "overlay" } />
+                        <div className={ "content center" }>
+                            <h2 style={ { paddingBottom: "20px" } }>{ event.name }</h2>
                         </div>
                     </div>
-                    <div style={{padding: "20px"}}>
-                        <Link className={"submit-btn"} to={"events/"+event.id+"/stands"}>Se stands</Link>
-                        <Link className={"submit-btn"} to={"events/edit/"+event.id}>Rediger</Link>
+                    <div style={ { padding: "20px" } }>
+                        <div style={ { paddingBottom: "20px" } }>
+                            <Link className={ "submit-btn" } to={ "events/" + event.id + "/stands" }>Se stands</Link>
+                            <Link className={ "submit-btn" } to={ "events/edit/" + event.id }>Rediger</Link>
+                        </div>
+                        <Link className={ "submit-btn" } to={ `jury?eventID=${ event.id }` }>Se vurderinger</Link>
                     </div>
-                </div>
-            })}
+                </div>) }
         </div>
-    </> : null;
-}
+    ) : null;
+};
 
 export default AdminEvents;
--- src/main/app/src/routes/adminRoutes.tsx
+++ src/main/app/src/routes/adminRoutes.tsx
@@ -1,17 +1,19 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AdminPage from "../pages/admin/AdminPage";
 import AdminEvent from "../pages/admin/AdminEvent";
 import AdminEventEdit from "../pages/admin/AdminEventEdit";
 import AdminStandEdit from "../pages/admin/AdminStandEdit";
 import AdminStandList from "../pages/admin/AdminStandList";
 import React from "react";
+import JuryPage from "../pages/jury/JuryPage";
 
 const adminRoutes = <Routes>
     <Route index element={ <AdminPage /> } />
     <Route path="/events" element={ <AdminEvent /> } />
     <Route path="/events/edit/:id" element={ <AdminEventEdit /> } />
     <Route path="/stand/edit/:id" element={ <AdminStandEdit /> } />
-    <Route path="/events/:id/stands" element={<AdminStandList/>}/>
-</Routes>
+    <Route path="/events/:id/stands" element={ <AdminStandList /> } />
+    <Route path={ "/jury" } element={ <JuryPage /> } />
+</Routes>;
 
 export default adminRoutes;
