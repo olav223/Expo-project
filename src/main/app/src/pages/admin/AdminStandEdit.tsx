f4b9e7fc51753adea609775943a876de28819605
Martin Berg Alstad <600878@stud.hvl.no>
2023-04-13 15:17:02 +0200

    Merget og lagt inn noen notifications. Lagt inn QR kode på nytt

--- src/main/app/src/pages/admin/AdminStandEdit.tsx
+++ src/main/app/src/pages/admin/AdminStandEdit.tsx
@@ -13,20 +13,13 @@ const AdminStandEdit = () => {
     const url = window.location.href.split("?")[1];
     const params = new URLSearchParams(url);
 
-    /*function QRCode(){
-        let url = 'https://api.qrserver.com/v1/create-qr-code/?data=http://ider-database.westeurope.cloudapp.azure.com:8080/Prosjekt-Expo-0.0.1-SNAPSHOT/stand?id='
-        return<div className="qr">
-            <img src={url + stand?.id} alt="QrCode"/>
-        </div>
-    }*/
-
-    return <>
+    return (
         <div style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", position: "relative"}}>
             <Backbtn />
             <h2>{id==="-1" ? "Lag stand" : "Rediger stand"}</h2>
-            <EditStand isAdd={id==="-1"} eventId={params.get("eventId")} getStandUrl={`/api/stand?id=${id}`} />
+            <EditStand isAdd={id==="-1"} eventId={parseInt(params.get("eventId")!)} getStandUrl={`/api/stand?id=${id}`} />
         </div>
-    </>
+    )
 }
 
 export default AdminStandEdit;
--- src/main/app/src/pages/exhibitor/ExhibitorPage.tsx
+++ src/main/app/src/pages/exhibitor/ExhibitorPage.tsx
@@ -1,41 +1,62 @@
-import {Link, Router, Routes, useLocation} from "react-router-dom";
-import React, {useEffect, useState} from "react";
-import ExhibitorEditPage from "./ExhibitorEditPage";
-// @ts-ignorem
-import axios, { AxiosError, AxiosResponse } from 'axios';
-import {StandModel} from "../../model/Stand";
+import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import type { StandModel } from "../../model/Stand";
 import restApi from "../../utils/restApi";
-import {useParams} from "react-router";
 import Auth from "../../utils/auth";
 import VotingStars from "../../components/VotingStars/VotingStars";
+import notification from "../../utils/notification";
 
+/**
+ * Hovedsiden til en utstiller. Viser et preview av standen deres, og en QR-kode som kan scannes for å komme til standen.
+ * Inneholder en knapp for å redigere standen sin.
+ * @constructor
+ */
 const ExhibitorPage = () => {
-    const [stand, setExhibitor] = useState<StandModel | null>(null)
+    const [stand, setExhibitor] = useState<StandModel | null>(null);
     const auth = new Auth();
 
     useEffect(() => {
         getStand();
     }, []);
 
-    const getStand = async() => {
-        const result = await restApi({url : `/api/admin/exhibitor/stand?exhibitor=${auth.getUser()?.username}`, method : "GET"})
+    const getStand = async () => {
+        const result = await restApi({
+            url: `/api/admin/exhibitor/stand?exhibitor=${ auth.getUser()?.username }`,
+            method: "GET"
+        });
 
-        if(result.status === 200){
-            setExhibitor(result.body)
+        if (result.status === 200) {
+            setExhibitor(result.body);
         }
+        else {
+            console.error(result);
+            notification({ type: "error", text: "Kunne ikke hente standen" });
+        }
+    };
+
+    function QRCode() {
+        const url = "https://api.qrserver.com/v1/create-qr-code/?data=http://ider-database.westeurope.cloudapp.azure.com:8080/Prosjekt-Expo-0.0.1-SNAPSHOT/stand?id=";
+        return (
+            <div className="qr">
+                <img src={ url + stand?.id } alt="QrCode" />
+            </div>
+        );
     }
 
-    return <div>
-        <Link style={{position: "absolute", right: "10px"}} to="edit">
-            <button type={"submit"}>Exhibitor edit page</button>
-        </Link>
-        <h1 style={{textAlign:"center"}}>Stand preview</h1>
-        <div className={"stand-info-parent box"}>
-            <h2>{stand?.title}</h2>
-            <VotingStars />
-            <p>{stand?.description}</p>
-            <img src={stand?.image} />
-        </div>
-    </div>
-}
+    return (
+        <>
+            <Link style={ { position: "absolute", right: "10px" } } to="edit">
+                <button type={ "submit" }>Rediger</button>
+            </Link>
+            <h1 style={ { textAlign: "center" } }>Stand forhåndsvisning</h1>
+            <div className={ "stand-info-parent box" }>
+                <h2>{ stand?.title }</h2>
+                <VotingStars />
+                <p>{ stand?.description }</p>
+                <img src={ stand?.image } alt={ "Bilde for stand" } />
+                <QRCode />
+            </div>
+        </>
+    );
+};
 export default ExhibitorPage;
