9fb5cb81f5c8feb2f4fb1da05efe8d885947f211
Tobias Lægreid <89257136+h600879@users.noreply.github.com>
2023-04-14 14:43:56 +0200

    Admin gets the correct events

--- src/main/app/src/components/AdminEvents/AdminEvents.tsx
+++ src/main/app/src/components/AdminEvents/AdminEvents.tsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import restApi from "../../utils/restApi";
 import { Link } from "react-router-dom";
 import "./AdminEvents.css";
+import Auth from "../../utils/auth";
 
 const AdminEvents = () => {
     const [events, setEvents] = useState<Array<EventModel>>([]);
+    const auth = new Auth();
 
     const getEvents = async () => {
-        const result = await restApi({ url: "/api/admin/events/all", method: "GET" });
+        const result = await restApi({ url: "/api/admin/events?username="+auth.getUser()?.username, method: "GET" });
         if (result.status === 200) {
             setEvents(result.body);
         }
@@ -20,10 +22,9 @@ const AdminEvents = () => {
         getEvents();
     }, []);
 
-    return events.length > 0 ? (
-        <div className={ "admin-events-parent" }>
+    return <div className={ "admin-events-parent" }>
             <h2>Hendelser du administrerer</h2>
-            { events.map((event) =>
+            { events.length > 0 ? events.map((event) =>
                 <div className={ "event-item box" } key={ event.id }>
                     <div className={ "image-layer" } style={ { backgroundImage: `url(${ event.image })` } }>
                         <div className={ "overlay" } />
@@ -38,9 +39,8 @@ const AdminEvents = () => {
                         </div>
                         <Link className={ "submit-btn" } to={ `jury?eventID=${ event.id }` }>Se vurderinger</Link>
                     </div>
-                </div>) }
+                </div>) : "Ingen events" }
         </div>
-    ) : null;
 };
 
 export default AdminEvents;
