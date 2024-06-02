961582ee1cafdfa4b3328d481add4faab3adf839
Stein Olav Løset Frey <steinolav98@hotmail.no>
2024-06-02 23:47:06 +0200

    added

--- src/main/app/src/index.tsx
+++ src/main/app/src/index.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from "./App";
+import {FpjsProvider} from "@fingerprintjs/fingerprintjs-pro-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-      <App />
+      <FpjsProvider
+          loadOptions={{
+           apiKey: process.env.FPJS_PUBLIC_API_KEY?? "",
+           region: "eu"
+          }}
+          >
+          <App />
+      </FpjsProvider>
   </React.StrictMode>
 );
